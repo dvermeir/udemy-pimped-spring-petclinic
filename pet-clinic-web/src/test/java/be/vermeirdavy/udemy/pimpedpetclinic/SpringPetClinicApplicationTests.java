fe253760e96589a9240d4ff9acf8805853440caa
davyvermeir_vdv <davy.vermeir@vandevelde.eu>
2023-02-05 20:35:50 +0100

    move to correct package

--- pet-clinic-web/src/test/java/be/vermeirdavy/udemy/pimpedpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/be/vermeirdavy/udemy/pimpedpetclinic/SpringPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package be.vermeirdavy.udemy.springpetclinic;
+package be.vermeirdavy.udemy.pimpedpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
